6bf6a4c0550204ba88a791289e8de67c6b21eb40
Javier Lopez-Gomez <36541918+jalopezg-git@users.noreply.github.com>
2024-05-27 15:18:39 +0200

    Apply minor fixes to `Makefile`s

--- src/make.mk
+++ src/make.mk
@@ -1,4 +1,7 @@
+# Target device
 P18FXXX = 18f452
+
 GPLIB = gplib
 AS = gpasm
-ASFLAGS = -I$(SRCDIR) -I$(SRCDIR)/../include -p $(P18FXXX) -w 0
+INCLUDEDIR ?= $(SRCDIR)/../include
+ASFLAGS = -I$(SRCDIR) -I$(INCLUDEDIR) -p $(P18FXXX) -w 0
