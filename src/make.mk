1a8f6cbe40c49ea180b742b8c7d58c2d200a4267
Javier Lopez-Gomez <36541918+jalopezg-git@users.noreply.github.com>
2024-05-31 21:27:47 +0200

    Expose `ENABLE_DOUBLE_BUFFER` build option

--- src/make.mk
+++ src/make.mk
@@ -5,3 +5,9 @@ GPLIB = gplib
 AS = gpasm
 INCLUDEDIR ?= $(SRCDIR)/../include
 ASFLAGS = -I$(SRCDIR) -I$(INCLUDEDIR) -p $(P18FXXX) -w 0
+
+# Optional features
+ifdef ENABLE_DOUBLE_BUFFER
+ENABLE_DOUBLE_BUFFER = 1
+endif
+ENABLE_DOUBLE_BUFFER ?= 0
